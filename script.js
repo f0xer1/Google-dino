5abdcb0c799a5b6d3c38422c65cd82a60a684d38
Pavlo Ilnitskyi <inavlo092@gmail.com>
2024-09-23 13:57:58 +0300

    Fix bugs, add more animation

--- script.js
+++ script.js
@@ -1,24 +1,22 @@
 const dino = document.getElementById('dino');
 const cactus = document.getElementById('cactus');
 
-document.addEventListener("keydown", function (event) {
+document.addEventListener("keydown", function () {
     jump();
 });
 
 function jump() {
-    if (dino.classList != 'jump') {
+    if (!dino.classList.contains('jump')) {
         dino.classList.add("jump");
+        setTimeout(function () {
+            dino.classList.remove('jump')
+        }, 700)
     }
-    setTimeout(function () {
-        dino.classList.remove('jump')
-    }, 600)
 }
-
-let isAlive = setInterval(function () {
+setInterval(function () {
     let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue('top'))
     let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue('left'))
-
-    if (cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 140) {
+    if (cactusLeft < 40 && cactusLeft > 0 && dinoTop >= 105) {
         alert("GEME OWER!!")
     }
 }, 10)
